60b29b261b6777695d5901beb4e2fbc1cd98757a
Duncan Tebbs <duncantebbs@turbulenz.com>
2013-08-05 18:28:35 +0900

    Change the build script to keep the different builds apart.  Don't build armv7s.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 # OpenSSL ${OPENSSL_VERSION} for iOS 5.1 that contains code for armv6, armv7 and i386.
 
 set -x
+set -e
 
 # Setup paths to stuff we need
 
@@ -11,7 +12,7 @@ OPENSSL_VERSION="1.0.1c"
 
 DEVELOPER="/Applications/Xcode.app/Contents/Developer"
 
-SDK_VERSION="6.0"
+SDK_VERSION="6.1"
 
 IPHONEOS_PLATFORM="${DEVELOPER}/Platforms/iPhoneOS.platform"
 IPHONEOS_SDK="${IPHONEOS_PLATFORM}/Developer/SDKs/iPhoneOS${SDK_VERSION}.sdk"
@@ -40,33 +41,34 @@ build()
    perl -i -pe 's|static volatile sig_atomic_t intr_signal|static volatile int intr_signal|' crypto/ui/ui_openssl.c
    perl -i -pe "s|^CC= gcc|CC= ${GCC} -arch ${ARCH}|g" Makefile
    perl -i -pe "s|^CFLAG= (.*)|CFLAG= -isysroot ${SDK} \$1|g" Makefile
-   make &> "/tmp/openssl-${OPENSSL_VERSION}-${ARCH}.log"
-   make install &> "/tmp/openssl-${OPENSSL_VERSION}-${ARCH}.log"
+   make -j 5
+   # &> "/tmp/openssl-${OPENSSL_VERSION}-${ARCH}.log"
+   make install
+   # &> "/tmp/openssl-${OPENSSL_VERSION}-${ARCH}.log"
    popd
    rm -rf "openssl-${OPENSSL_VERSION}"
 }
 
+# ios
+
 build "armv7" "${IPHONEOS_GCC}" "${IPHONEOS_SDK}"
-build "armv7s" "${IPHONEOS_GCC}" "${IPHONEOS_SDK}"
+mkdir -p lib/ios
+cp /tmp/openssl-${OPENSSL_VERSION}-armv7/lib/libcrypto.a \
+  /tmp/openssl-${OPENSSL_VERSION}-armv7/lib/libssl.a \
+  lib/ios
+
+# iossim
+
 build "i386" "${IPHONESIMULATOR_GCC}" "${IPHONESIMULATOR_SDK}"
+mkdir -p lib/iossim
+cp /tmp/openssl-${OPENSSL_VERSION}-i386/lib/libcrypto.a \
+  /tmp/openssl-${OPENSSL_VERSION}-i386/lib/libssl.a \
+  lib/iossim
 
-#
+# includes
 
-mkdir include
+mkdir -p include
 cp -r /tmp/openssl-${OPENSSL_VERSION}-i386/include/openssl include/
-
-mkdir lib
-lipo \
-	"/tmp/openssl-${OPENSSL_VERSION}-armv7/lib/libcrypto.a" \
-	"/tmp/openssl-${OPENSSL_VERSION}-armv7s/lib/libcrypto.a" \
-	"/tmp/openssl-${OPENSSL_VERSION}-i386/lib/libcrypto.a" \
-	-create -output lib/libcrypto.a
-lipo \
-	"/tmp/openssl-${OPENSSL_VERSION}-armv7/lib/libssl.a" \
-	"/tmp/openssl-${OPENSSL_VERSION}-armv7s/lib/libssl.a" \
-	"/tmp/openssl-${OPENSSL_VERSION}-i386/lib/libssl.a" \
-	-create -output lib/libssl.a
 
 rm -rf "/tmp/openssl-${OPENSSL_VERSION}-*"
 rm -rf "/tmp/openssl-${OPENSSL_VERSION}-*.log"
-
